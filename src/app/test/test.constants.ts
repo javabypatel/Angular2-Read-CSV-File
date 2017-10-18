0b52a1c2c37786c2d7ed0effa04e4004712ede3b
Jayesh Patel <git>
2017-10-18 19:26:09 +0530

    added sample .csv file to test and separate constants file for csv validation configurations.

---
+++ src/app/test/test.constants.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Constants {
+    static tokenDelimeter = ",";
+    static isHeaderPresentFlag = true;
+    static validateHeaderAndRecordLengthFlag = true;
+    static valildateFileExtenstionFlag = true;
+}
